50925de57b3462b84733c7cd361fdc4726a3832d
szymek1 <bogusszymon2000@gmail.com>
2025-10-30 22:20:58 +0100

    Added edge_detector component to drone_pkg and bassy3 hardware parameters

--- src/hdl/drone_utils_pkg.vhd
+++ src/hdl/drone_utils_pkg.vhd
@@ -23,3 +23,29 @@
 -- Additional Comments:
 -- 
 -----------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+
+package drone_utils_pkg is
+    -- General hardware parameters
+    constant C_BASYS3_SYSCLK_HZ: integer := 50_000_000; -- system clock frequency (50MHz)
+    constant C_PWM_CLK_HZ      : integer := 5_000;      -- target PWM frequency (5kHz)
+
+    -- Edge detector
+    component edge_detector is
+        generic (
+            G_RISING_EDGE: boolean := true; -- detect rising edge by default
+        );
+        port (
+            i_clk: in std_logic;
+            i_rst_n: in std_logic;
+            i_signal: in std_logic;
+            o_edge: out std_logic -- set to high for one clock cycle
+        );
+    end component; -- end of edge_detector
+    
+
+end package drone_utils_pkg; -- end of the package
